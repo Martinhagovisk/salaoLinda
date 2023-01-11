1697896a79d921790a3db06bc650588346e55c0f
Martinhagovisk <108485691+Martinhagovisk@users.noreply.github.com>
2023-01-11 15:41:34 -0300

    Update GerenciadorSalao.java

--- salaoLinda/src/br/com/linda/classes/GerenciadorSalao.java
+++ salaoLinda/src/br/com/linda/classes/GerenciadorSalao.java
@@ -11,3 +11,36 @@ package br.com.linda.classes;
 public class GerenciadorSalao implements Gerenciador {
     
 }
+public class GerenciadorSalao implements Gerenciador {
+    List<Cliente> clientes;
+    List<Servico> servicos;
+    // Implementação dos métodos abstratos
+    public void adicionarCliente(Cliente c) {
+        clientes.add(c);
+    }
+    public void removerCliente(Cliente c) {
+        clientes.remove(c);
+    }
+    public Cliente pesquisarCliente(String nome) {
+        for (Cliente c : clientes) {
+            if (c.getNome().equals(nome)) {
+                return c;
+            }
+        }
+        return null;
+    }
+    public void adicionarServico(Servico s) {
+        servicos.add(s);
+    }
+    public void removerServico(Servico s) {
+        servicos.remove(s);
+    }
+    public Servico pesquisarServico(String nome) {
+        for (Servico s : servicos) {
+            if (s.getNome().equals(nome)) {
+                return s;
+            }
+        }
+        return null;
+    }
+}
